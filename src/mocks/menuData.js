71687fcf7ae655cf7c532eeb145591d13f5e78db
AndreyChenzov242 <andreychenzov@gmail.com>
2020-09-15 21:35:13 +0300

    clear code

---
+++ src/mocks/menuData.js
@@ -0,0 +1,255 @@
+import pizza4Seasons from '../assets/images/pizza-4-seasons.jpg';
+import pizzaBavar from '../assets/images/pizza-bavar.jpg';
+import pizzaFermers from '../assets/images/pizza-fermers.jpg';
+import pizzaHawaiian from '../assets/images/pizza-gavaysk.jpg';
+import pizzaKaprichoze from '../assets/images/pizza-kaprichoze.jpg';
+
+import pastakarbonara from '../assets/images/pasta-karbonara.jpg';
+import pastaSalmon from '../assets/images/pasta-salmon.jpg';
+import pastaTalyatte from '../assets/images/pasta-talyatte.jpg';
+import pastaTalyatteChiken from '../assets/images/pasta-talyatte-chiken.jpg';
+import pastaVeal from '../assets/images/pasta-veal.jpg';
+import pastaBecon from '../assets/images/pasta-becon.jpg';
+import pastaShrimp from '../assets/images/pasta-shrimp.jpg';
+
+import soupChiken from '../assets/images/soup-chiken.jpg';
+import soupFadjoli from '../assets/images/soup-fadjoli.jpg';
+import soupKachuko from '../assets/images/soup-kachuko.jpg';
+import soupMinitrone from '../assets/images/soup-minitrone.jpg';
+
+import saladGreece from '../assets/images/salad-grece.jpg';
+import saladKarpeze from '../assets/images/salad-karpeze.jpg';
+import saladLanget from '../assets/images/salad-langet.jpg';
+import saladMeal from '../assets/images/salad-meal.jpg';
+import saladSalmon from '../assets/images/salad-salmon.jpg';
+import saladCaesar from '../assets/images/salad-tsezar.jpg';
+
+export const menuData = [
+  {
+    id: '1',
+    title: 'Four seasons',
+    name: 'pizza',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor do eiusmod tempor do eiusmod tempor',
+    img: pizza4Seasons,
+    price: '31',
+    counter: 0,
+  },
+  {
+    id: '2',
+    title: 'Bavarian pizza with large volcanic sides',
+    name: 'pizza',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor do eiusmod tempor do eiusmod tempor',
+    img: pizzaBavar,
+    price: '25',
+    counter: 0,
+  },
+  {
+    id: '3',
+    title: 'Farm',
+    name: 'pizza',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pizzaFermers,
+    price: '23',
+    counter: 0,
+  },
+  {
+    id: '4',
+    title: 'Hawaiian',
+    name: 'pizza',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pizzaHawaiian,
+    price: '30',
+    counter: 0,
+  },
+  {
+    id: '5',
+    title: 'Kaprichoze',
+    name: 'pizza',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pizzaKaprichoze,
+    price: '22',
+    counter: 0,
+  },
+  {
+    id: '6',
+    title: 'Karbonara',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastakarbonara,
+    price: '12',
+    counter: 0,
+  },
+  {
+    id: '7',
+    title: 'Shrimp',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaShrimp,
+    price: '10',
+    counter: 0,
+  },
+  {
+    id: '8',
+    title: 'Salmon',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaSalmon,
+    price: '8',
+
+    counter: 0,
+  },
+  {
+    id: '9',
+    title: 'Becon',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaBecon,
+    price: '17',
+    counter: 0,
+  },
+  {
+    id: '10',
+    title: 'Tagliatelle',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaTalyatte,
+    price: '15',
+    counter: 0,
+  },
+  {
+    id: '11',
+    title: 'Veal',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaVeal,
+    price: '11',
+    counter: 0,
+  },
+  {
+    id: '12',
+    title: 'Tagliatelle with chicken',
+    name: 'pasta',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: pastaTalyatteChiken,
+    price: '22',
+    counter: 0,
+  },
+  {
+    id: '13',
+    title: 'Chiken',
+    name: 'soups',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: soupChiken,
+    price: '8',
+
+    counter: 0,
+  },
+  {
+    id: '14',
+    title: 'Fadjoli',
+    name: 'soups',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: soupFadjoli,
+    price: '7',
+
+    counter: 0,
+  },
+
+  {
+    id: '15',
+    title: 'Kachuko',
+    name: 'soups',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: soupKachuko,
+    price: '5',
+
+    counter: 0,
+  },
+  {
+    id: '16',
+    title: 'Minitrone',
+    name: 'soups',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: soupMinitrone,
+    price: '10',
+    counter: 0,
+  },
+  {
+    id: '17',
+    title: 'Greece',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladGreece,
+    price: '15',
+    counter: 0,
+  },
+  {
+    id: '18',
+    title: 'Karpeze',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladKarpeze,
+    price: '12',
+    counter: 0,
+  },
+
+  {
+    id: '19',
+    title: 'Langet',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladLanget,
+    price: '25',
+    counter: 0,
+  },
+  {
+    id: '20',
+    title: 'Meal',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladMeal,
+    price: '16',
+    counter: 0,
+  },
+  {
+    id: '21',
+    title: 'Salmon',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladSalmon,
+    price: '9',
+
+    counter: 0,
+  },
+  {
+    id: '22',
+    title: 'Caesar',
+    name: 'salads',
+    description:
+      'Lorem ipsum dolor amet consectetur  adipisicingsed do eiusmod tempor',
+    img: saladCaesar,
+    price: '16',
+    counter: 0,
+  },
+];
